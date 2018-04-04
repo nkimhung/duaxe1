614eceb20ada27383472d03308e33d5aaf40e5dc
nkimhung <37534941+nkimhung@users.noreply.github.com>
2018-04-04 23:04:58 +0700

    Update duaxe.cpp

--- duaxe.cpp
+++ duaxe.cpp
@@ -5,6 +5,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
 using namespace std;
 #define consoleWidth 80
 #define consoleHeight 25
@@ -70,13 +71,15 @@ struct xe
 {
     toado td;
     hinhdang hd;
-    int run=0;
+    int run=0,vacham=0,sovcqua=0,life=0;
 };
 struct vc
 {
-    toado td;
+    toado td,td2,td3;
+
+
 };
-void khoitao(xe &xe,vc &vc){
+void khoitao(xe &xe,vc vc[3]){
     int i;
  xe.td.y=consoleHeight-3;
  xe.td.x=ROAD/2;
@@ -91,11 +94,15 @@ void khoitao(xe &xe,vc &vc){
  xe.hd.a[1][2]='|';
  /*for(i=0;i<=2;i++)
  {*/
- vc.td.x=3+rand()%36;
- vc.td.y=-2;
- /*}*/
+ for(i=0;i<=2;i++)
+ {vc[i].td.x=3+rand()%36;
+ vc[i].td.y=-2-i*7;
  }
-void hienthi(const xe &xe,const vc &vc,char a[25][41]){
+ /*}*/
+ vc[1].td2.x=4+rand()%34;
+ vc[1].td2.y=-50;
+ }
+void hienthi(const xe &xe,const vc vc[3],char a[25][41]){
 	int kdong,kcot,x,y,i;
 
 	for(kdong=-1;kdong<=1;kdong++)
@@ -105,19 +112,42 @@ void hienthi(const xe &xe,const vc &vc,char a[25][41]){
             a[y-1][x-1]=xe.hd.a[kdong+1][kcot+1];
 			}
 
+        for(i=0;i<=2;i++)
     	for(kdong=-1;kdong<=1;kdong++)
         	{
-            	x=vc.td.x-1;
-            	y=kdong+vc.td.y;
+            	x=vc[i].td.x-1;
+            	y=kdong+vc[i].td.y;
                 if(y>0&&y<26){
             		a[y-1][x-1]=33;
             		a[y-1][x]=33;
             		a[y-1][x+1]=33;}
 			}
+
+    y=vc[1].td2.y-1;
+    x=vc[1].td2.x-1;
+    if(y>0&&y<25)
+        {
+            a[y][x-2]='+';
+            a[y][x-1]='L';
+            a[y][x]='I';
+            a[y][x+1]='F';
+            a[y][x+2]='E';
+        }
+
+
+
     gotoXY(0,0);
+    TextColor(70);
     cout<<a[0];
-    gotoXY(40,22);
-    cout<<"              ";
+    gotoXY(55,12);
+    cout<<" SO VA CHAM: "<<xe.vacham;
+    gotoXY(55,13);
+    cout<<" SO VAT CAN QUA: "<<xe.sovcqua;
+    gotoXY(55,14);
+    cout<<" MANG SONG: "<<xe.life;
+
+    /*gotoXY(40,24);
+    cout<<"              ";*/
 
 }
 
@@ -142,13 +172,23 @@ void dieukhien(xe &xe){
     }
 }
 
-void xoa( const xe &xe,const vc&vc,char a[25][41]){
+void xoa( const xe &xe,const vc vc[3],char a[25][41]){
+int i;
+for(i=0;i<=2;i++)
+        if(vc[i].td.y>=3 && vc[i].td.y<28)
+        {
+            a[vc[i].td.y-3][vc[i].td.x-2]=32;
+            a[vc[i].td.y-3][vc[i].td.x-1]=32;
+            a[vc[i].td.y-3][vc[i].td.x]=32;
+        }
 
-    if(vc.td.y>=3 && vc.td.y<28)
+if(vc[1].td2.y>=2&&vc[1].td2.y<=26)
     {
-    a[vc.td.y-3][vc.td.x-2]=32;
-    a[vc.td.y-3][vc.td.x-1]=32;
-    a[vc.td.y-3][vc.td.x]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x-3]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x-2]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x-1]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x+1]=32;
     }
 switch(xe.run){
 case 3:
@@ -174,29 +214,75 @@ case 2:
 
 }
 }
-void xl(vc &vc){
-vc.td.y++;
-if(vc.td.y>27) {
-vc.td.x=3+rand()%36;
- vc.td.y=-2;}
+void xl(vc vc[3],xe &xe,char a[25][41]){
+int i,dx,dy,kdong,x,y;
+
+vc[0].td.y++;
+vc[1].td.y++;
+vc[2].td.y++;
+vc[1].td2.y++;
+
+for(i=0;i<=2;i++) {
+    dx=xe.td.x-vc[i].td.x;
+    dy=xe.td.y-vc[i].td.y;
+    if(abs(dx)<=2&&abs(dy)<=2)
+    {   xe.vacham++;
+        for(kdong=-1;kdong<=1;kdong++)
+        	{
+            	x=vc[i].td.x-1;
+            	y=kdong+vc[i].td.y;
+                if(y>=2&&y<26){
+            		a[y-2][x-1]=32;
+            		a[y-2][x]=32;
+            		a[y-2][x+1]=32;}
+			}
+        vc[i].td.x=3+rand()%36;
+        vc[i].td.y=-2;
+    }
+    if(vc[i].td.y>27) {
+        vc[i].td.x=3+rand()%36;
+        vc[i].td.y=-2;
+        xe.sovcqua++;
+                        }
+}
+dx=xe.td.x-vc[1].td2.x;
+dy=xe.td.y-vc[1].td2.y;
+if(abs(dx)<=3&&abs(dy)<=1){
+    xe.life++;
+    if(vc[1].td2.y>=2&&vc[1].td2.y<=26){
+    a[vc[1].td2.y-2][vc[1].td2.x-3]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x-2]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x-1]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x]=32;
+    a[vc[1].td2.y-2][vc[1].td2.x+1]=32;}
+    vc[1].td2.x=4+rand()%34;
+    vc[1].td2.y=-50;
+
+}
+
+if(vc[1].td2.y>26)
+    {
+    vc[1].td2.x=4+rand()%34;
+    vc[1].td2.y=-50;
+    }
+
 }
 int main(){
     xe xe;
-    vc vc;
+    vc vc[3];
     char a[25][41];
     khoitao1(a);
     srand(time(NULL));
     ShowCur(false);
     khoitao(xe,vc);
     while (1){
-		gotoXY(0,0);
         hienthi(xe,vc,a);
 
         dieukhien(xe);
-        xl(vc);
+        xl(vc,xe,a);
         xoa(xe,vc,a);
 
-        Sleep(200);
+        Sleep(100);
     }
 
 }
