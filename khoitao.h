a348071f84479f0ff8af9ae2f26dafe755c618cc
nkimhung <37534941+nkimhung@users.noreply.github.com>
2018-04-25 23:27:28 +0700

    Update khoitao.h

--- khoitao.h
+++ khoitao.h
@@ -1,1 +1,13 @@
+#ifndef _khoitao
+#define _khoitao
+#define ROAD 41
+#include"cackieudulieumoi.h"
+
+//tao hinh dang duong dua
+void khoitao1(char (*)[ROAD+1] );
+
+//tao hinh dang ,vi tri cac vat tren duong dua
+void khoitao(xedua &,vatcan*);
+
+#endif // _khoitao
 
