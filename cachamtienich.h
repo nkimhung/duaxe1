3039051843d5376b69defd5e5f9ead2062faac74
nkimhung <37534941+nkimhung@users.noreply.github.com>
2018-04-25 23:18:05 +0700

    Update cachamtienich.h

--- cachamtienich.h
+++ cachamtienich.h
@@ -1,1 +1,30 @@
+#ifndef _hamtienich
+#define _hamtienich
+#include<windows.h>
+#define key_Up		1072
+#define key_Down	1080
+#define key_Left	1075
+#define key_Right	1077
+#define key_none	-1
+
+//cai dat mau chu,mau nen
+void TextColor (int );
+
+//dua con tro toi vi tri tuong ung
+void gotoXY(int,int);
+
+//an,hien con tro tren man hinh
+void ShowCur(bool );
+
+//kiem tra viec nhap ban phim
+int inputKey();
+
+//xoa man hinh
+void clrscr();
+#endif // _hamtienich
+
+
+
+
+
 
