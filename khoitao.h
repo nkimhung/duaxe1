cd53b0dba621d600d1bc62a84d98ef527c0a068a
nkimhung <37534941+nkimhung@users.noreply.github.com>
2018-05-11 13:46:09 +0700

    Update khoitao.h

--- khoitao.h
+++ khoitao.h
@@ -1,13 +1,18 @@
-#ifndef _khoitao
-#define _khoitao
-#define ROAD 41
-#include"cackieudulieumoi.h"
+#ifndef _KHOITAO
+#define _KHOITAO
+#include"kieudulieumoi.h"
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+//huong dan dau game
+string Help();
 
 //tao hinh dang duong dua
-void khoitao1(char (*)[ROAD+1] );
+void initialize_racetrack(char (*)[ROAD+1] );
 
 //tao hinh dang ,vi tri cac vat tren duong dua
-void khoitao(xedua &,vatcan*);
+void initialize_location(Race_Car &,hindrance*,hindrance &);
 
-#endif // _khoitao
+#endif // _KHOITAO
+
 
