cd958d759a0367f7c0aba0d6fdeee65493da72a3
nkimhung <37534941+nkimhung@users.noreply.github.com>
2018-05-11 13:31:18 +0700

    Update cachamsulychinh.cpp

--- cachamsulychinh.cpp
+++ cachamsulychinh.cpp
@@ -1,297 +1,223 @@
-#include"cachamsulychinh.h"
-#include"hamtienich.h"
-#include"cackieudulieumoi.h"
-#include<iostream>
-#include<string.h>
-#include<stdlib.h>
-using namespace std;
-#define consoleHeight 25
-#define ROAD 41
-void Update(xedua &xe,const vatcan vc[3],char a[consoleHeight][ROAD+1]){
-    int kdong,kcot,x,y,i;
-    //gan hinh dang va vi tri xe vao mang
-	for(kdong=-1;kdong<=1;kdong++)
-        for(kcot=-1;kcot<=1;kcot++){
-            x=kcot+xe.td.column;
-            y=kdong+xe.td.line;
-            a[y-1][x-1]=xe.hd.a[kdong+1][kcot+1];
-			}
-    //kt viec nhan phim ban dan
-    if(xe.ban==1){
-    x=xe.td.column-1;
-    y=xe.td.line-3;
+#include"cachamxulyxechinh.h"
 
-    //kt xem con dan trong xe khong
-    if(xe.dan>0){
-        //gan duong dan vao mang
-        while(y>=0){
-            a[y][x]=177;
-            y--;
-                }
-        //giam so vien dan di
-        xe.dan--;
-        xe.affirmation[0]=true;//xac nhan su thay doi cua dan
-                    }
-    xe.ban=0;
-    }
+//Hien thi tro choi
+void Display_game( Race_Car &car,char racetrack[consoleHeight][ROAD+1])
+{
 
-    for(i=0;i<=2;i++)
-    for(kdong=-1;kdong<=1;kdong++)
-    {
-        x=vc[i].td.column-1;
-        y=kdong+vc[i].td.line;
-        if(y>0&&y<26){
-            a[y-1][x-1]=219;
-            a[y-1][x]=219;
-            a[y-1][x+1]=219;}
-    }
-    //gan vi tri hinh dang life vao mang
-    y=vc[1].td2.line-1;
-    x=vc[1].td2.column-1;
-    if(y>0&&y<25)
-        {
-            a[y][x-2]='+';
-            a[y][x-1]='L';
-            a[y][x]='I';
-            a[y][x+1]='F';
-            a[y][x+2]='E';
-        }
-
-
-
-}
-void Delete(xedua &xe, const vatcan vc[3],char a[consoleHeight][ROAD+1]){
-    int kdong,kcot,x,y,i;
-    //xoa xe
-    for(kdong=-1;kdong<=1;kdong++)
-        for(kcot=-1;kcot<=1;kcot++){
-            x=kcot+xe.td.column;
-            y=kdong+xe.td.line;
-            a[y-1][x-1]=32;
-			}
-
-    x=xe.td.column-1;//vi tri cot cua duong dan trong mang
-    y=xe.td.line-3;//vi tri dong cua duong dan trong mang
-    //xoa duong dan
-    while(y>=0){
-        a[y][x]=32;
-        y--;
-                }
-    //xoa cac vat can
-    for(i=0;i<=2;i++)
-    	for(kdong=-1;kdong<=1;kdong++)
-        	{
-            	x=vc[i].td.column-1;
-            	y=kdong+vc[i].td.line;
-                if(y>0&&y<=consoleHeight){
-            		a[y-1][x-1]=32;
-            		a[y-1][x]=32;
-            		a[y-1][x+1]=32;}
-			}
-
-    //xoa phep bo tro life
-    y=vc[1].td2.line-1;
-    x=vc[1].td2.column-1;
-    if(y>0&&y<consoleHeight)
-        {
-            a[y][x-2]=32;
-            a[y][x-1]=32;
-            a[y][x]=32;
-            a[y][x+1]=32;
-            a[y][x+2]=32;
-        }
-
-
-}
-void Display( xedua &xe,const vatcan vc[3],char a[consoleHeight][ROAD+1]){
-
-
-    TextColor(70);
+    TextColor(30);
     gotoXY(0,0);
-    //hien thi tro choi
-    cout<<a[0];
+    cout<<racetrack[0];
+    //Hien thi so dan
+    if(car.getAff(0))
+    {
+        gotoXY(55,12);
+        cout<<" DAN:      "<<car.getDan()<<" ";
+        car.setAff(false,0);
+    }
 
-    //hien thi so dan
-    if(xe.affirmation[0])
-    {   gotoXY(65,12);
-        cout<<xe.dan<<"      ";
-        xe.affirmation[0]=false;}
+    //Hien thi so vat can qua
+    if(car.getAff(1))
+    {
+        gotoXY(55,13);
+        cout<<" SO VAT CAN QUA:    "<<car.getSovcqua();
+        car.setAff(false,1);
+    }
 
-    //hien thi so vat can qua
-    if(xe.affirmation[1]){
-        gotoXY(75,13);
-        cout<<xe.sovcqua;
-        xe.affirmation[1]=false;}
-
-    //hien thi so mang
-    if(xe.affirmation[2]){
-        gotoXY(70,14);
-        cout<<xe.life<<"       ";
-        xe.affirmation[2]=false;}
+    //Hien thi so mang
+    if(car.getAff(2))
+    {
+        gotoXY(55,14);
+        cout<<" MANG SONG:      "<<car.getLife()<<" ";
+        car.setAff(false,2);
+    }
 
 
 }
 
-void Control(xedua &xe){
+//Dieu khien xe
+void Control_car(Race_Car &car)
+{
     int i;
+    coordinate Td;
+    Td=car.getCoordinate();
     i=inputKey();
-    if(key_Up==i&&xe.td.line>=3)
-        xe.td.line--;
-    else
-        if(key_Down==i&&xe.td.line<=consoleHeight-2)
-            xe.td.line++;
-        else
-            if(key_Left==i&&xe.td.column>=6)
-                xe.td.column-=3;
-            else
-                if(key_Right==i&& xe.td.column<=ROAD-5)
-                    xe.td.column+=3;
-                else
-                    if(i==32)
-                        xe.ban=1;
-                    else
-                        if(i=='s'||i=='S')
-                            xe.stop=true;
+    if(key_Up==i&&Td.row>=3)//xe di xuong
+        car.addCoordinate(0,-1);
+    else if(key_Down==i&&Td.row<=consoleHeight-2) //xe di len
+        car.addCoordinate(0,1);
+    else if(key_Left==i&&Td.column>=6) //xe sang trai
+        car.addCoordinate(-3,0);
+    else if(key_Right==i&& Td.column<=ROAD-5) //xe sang phai
+        car.addCoordinate(3,0);
+    else if(i==32) //ban dan
+        car.setBan(true);
+    else if(i=='s'||i=='S') //tam dung
+        car.setStop(true);
 }
 
-void xuly(vatcan vc[3],xedua &xe,char a[consoleHeight][ROAD+1]){
+//Xu ly cac truong hop voi xe
+void handle_car(Race_Car &car,hindrance dike[3],hindrance &life)
+{
     int i,dx,dy,kdong,x,y;
 
-    vc[0].td.line++;
-    vc[1].td.line++;
-    vc[2].td.line++;
-    vc[1].td2.line++;
+    dike[0].td.row++;
+    dike[1].td.row++;
+    dike[2].td.row++;
+    life.td.row++;
 
-    //xu ly cac tinh huong xay ra voi vat can
-    for(i=0;i<=2;i++) {
-        dx=xe.td.column-vc[i].td.column;//khoang cach cot giua xe voi vat can
-        dy=xe.td.line-vc[i].td.line;//khoang cach dong giua xe voi vat can
+    //Xu ly cac tinh huong xay ra voi vat can
+
+    coordinate Td;
+    Td=car.getCoordinate();
+
+    for(i=0; i<=2; i++)
+    {
+        dx=Td.column-dike[i].td.column;//khoang cach cot giua xe voi vat can
+        dy=Td.row-dike[i].td.row;//khoang cach hang giua xe voi vat can
         if(abs(dx)<=2&&abs(dy)<=2) //kiem tra su vat cham giua xe voi vat can
         {
-            if(xe.life>0){
-                xe.life--;
-                xe.affirmation[2]=1;
-                vc[i].td.column=3+rand()%(ROAD-4);
-                vc[i].td.line=-2;
-                        }
+            if(car.getLife()>0)
+            {
+                car.addLife(-1);
+                car.setAff(true,2);
+                dike[i].td.column=3+rand()%(ROAD-4);
+                dike[i].td.row=-2;
+            }
             else
-                xe.loss=true;
+                car.setLoss(true);
         }
 
-    //kiem tra neu dan co ban vao vat can thi khoi tao lai toa do vat can
-        if(xe.ban==1&&abs(dx)<=2&&xe.dan>0){
-            vc[i].td.column=3+rand()%(ROAD-4);
-            vc[i].td.line=-2;
-                                            }
-
-    //kiem tra neu vat can di het duong dua thi update lai thong tin
-        if(vc[i].td.line>27) {
-            vc[i].td.column=3+rand()%(ROAD-4);
-            vc[i].td.line=-2;
-            xe.sovcqua++;
-            xe.affirmation[1]=true;
-                        }
+        //Kiem tra neu dan co ban vao vat can thi khoi tao lai toa do vat can
+        if(car.getBan()&&abs(dx)<=2&&car.getDan()>0)
+        {
+            dike[i].td.column=3+rand()%(ROAD-4);
+            dike[i].td.row=-2;
         }
 
-    //xu ly cac tinh huong voi phep bo tro life
-    dx=xe.td.column-vc[1].td2.column;//khoang cach cot giua xe voi phep bo tro mang song
-    dy=xe.td.line-vc[1].td2.line;//khoang cach dong giua xe voi phep bo tro mang song
-    if(abs(dx)<=3&&abs(dy)<=1){//kiem tra su an phep bo tro cua xe
-        xe.life++;
-        xe.affirmation[2]=true;
-        //update lai toa do
-        vc[1].td2.column=4+rand()%(ROAD-6);
-        vc[1].td2.line=-50;
-                            }
-    if(xe.ban==1&&abs(dx)<=3&&xe.dan>0){//kiem tra xem dan co ban vao phep bo tro khong
-        vc[1].td2.column=4+rand()%(ROAD-6);
-        vc[1].td2.line=-50;
-                                        }
-    if(vc[1].td2.line>26)//kiem tra xem phep bo tro mang song da di het duong dua chua
+        //Kiem tra neu vat can di het duong dua thi update lai thong tin
+        if(dike[i].td.row>27)
+        {
+            dike[i].td.column=3+rand()%(ROAD-4);
+            dike[i].td.row=-2;
+            car.addSovcqua(1);
+            car.setAff(true,1);
+
+        }
+    }
+
+    //Xu ly cac tinh huong voi phep bo tro life
+    dx=Td.column-life.td.column;//khoang cach cot giua xe voi phep bo tro mang song
+    dy=Td.row-life.td.row;//khoang cach hang giua xe voi phep bo tro mang song
+    if(abs(dx)<=3&&abs(dy)<=1) //kiem tra viec an phep bo tro cua xe
     {
-        vc[1].td2.column=4+rand()%(ROAD-6);
-        vc[1].td2.line=-50;
+        car.addLife(1);
+        car.setAff(true,2);
+
+        //update lai toa do
+        life.td.column=4+rand()%(ROAD-6);
+        life.td.row=-50;
+    }
+    if(car.getBan()&&abs(dx)<=3&&car.getDan()>0) //kiem tra xem dan co ban vao phep bo tro khong
+    {
+        life.td.column=4+rand()%(ROAD-6);
+        life.td.row=-50;
+    }
+    if(life.td.row>26)//kiem tra xem phep bo tro mang song da di het duong dua chua
+    {
+        life.td.column=4+rand()%(ROAD-6);
+        life.td.row=-50;
     }
 
 }
-void xulyluachon(xedua &xe,vatcan vc[3]){
-while(xe.loss) {
 
-                clrscr();
-                cout<<" nhap a de tiep tuc x the thoat"<<endl;
-                string key;
-                cin>>key;
-                if(key=="a"||key=="A") {
-                    int i;
+//Xu ly lua chon nguoi choi khi thua cuoc
+void Solve_The_Selection(Race_Car &car,hindrance dike[3],hindrance &life)
+{
+    while(car.getLoss())
+    {
 
-                    //update lai vi tri ban dau cua xe
-                    xe.td.line=consoleHeight-3;
-                    xe.td.column=ROAD/2+1;
+        clrscr();
+        cout<<" Nhan phim \"A\" de choi lai \"X\" de thoat \n\n Nhan phim Enter de xac nhan"<<endl;
+        string key;
+        fflush(stdin);
+        cin>>key;
+        if(key=="a"||key=="A")
+        {
+            int i;
 
-                    //update lai vi tri ban dau cua vat can
-                    for(i=0;i<=2;i++)
-                    {
-                        vc[i].td.column=3+rand()%37;
-                        vc[i].td.line=-2-i*7;
-                    }
-                    //update lai vi tri ban dau cua phep bo tro láº¡i life
-                    vc[1].td2.column=4+rand()%35;
-                    vc[1].td2.line=-50;
+            //update lai vi tri ban dau cua xe
+            car.setCoordinate(ROAD/2+1,consoleHeight-3);
 
-                    //in ra thong tin ban dau game
-                    if(xe.sovcqua>xe.sovcquamax) xe.sovcquamax=xe.sovcqua;
-                    gotoXY(47,11);
-                    cout<<"SO VAT CAN QUA NHIEU NHAT:"<<xe.sovcquamax;
-                    gotoXY(55,12);
-                    cout<<" DAN:      0";
-                    gotoXY(55,13);
-                    cout<<" SO VAT CAN QUA:    0";
-                    gotoXY(55,14);
-                    cout<<" MANG SONG:      0";
-                    gotoXY(0,0);
-                    xe.dan=0;
-                    xe.life=0;
-                    xe.sovcqua=0;
-                    xe.tem=0;
-
-
-                    xe.loss=false;
-                }
-                else
-                    if(key=="x"||key=="X")
-                        exit(0);
-
+            //update lai vi tri ban dau cua vat can
+            for(i=0; i<=2; i++)
+            {
+                dike[i].td.column=3+rand()%37;
+                dike[i].td.row=-2-i*7;
             }
+            //update lai vi tri ban dau cua phep bo tro life
+            life.td.column=4+rand()%35;
+            life.td.row=-50;
+            //cap nhap diem cao nhat
+            if(car.getSovcqua()>car.getSovcquaMax())
+                car.setSovcquaMax(car.getSovcqua());
+
+            //in ra them diem cao nhat
+            gotoXY(47,11);
+            cout<<"SO VAT CAN QUA NHIEU NHAT:"<<car.getSovcquaMax();
+
+            //thiet lap trang thai dau game
+            car.setAff( true,0);
+            car.setAff(true,1);
+            car.setAff(true,2);
+            car.setDan(0);
+            car.setLife(0);
+            car.setSovcqua(0);
+            car.setDem_CK_Dan(0);
+            car.setDem_CK_Life(0);
+            car.setTocDo(50);
+            car.setLoss(false);
+        }
+        else if(key=="x"||key=="X")
+        {
+            TextColor(10);
+            cout<<"\nGAME OVER!"<<endl;
+            exit(0);
+        }
+    }
 }
-void dungtrochoi(xedua &xe){
-    while(xe.stop) {
-                clrscr();
-                cout<<"Nhap a de tiep tuc choi x the thoat"<<endl;
-                string key;
-                cin>>key;
-                if(key=="a"||key=="A") {
 
-                    //in ra thong hien tai cua game
-                    gotoXY(47,11);
-                    cout<<"SO VAT CAN QUA NHIEU NHAT:"<<xe.sovcquamax;
-                    gotoXY(55,12);
-                    cout<<" Dan:      0";
-                    gotoXY(55,13);
-                    cout<<" SO VAT CAN QUA:    0";
-                    gotoXY(55,14);
-                    cout<<" MANG SONG:      0";
-                    gotoXY(0,0);
-                    xe.affirmation[0]=true;
-                    xe.affirmation[1]=true;
-                    xe.affirmation[2]=true;
-
-
-                    xe.stop=false;
-                                        }
-                else
-                    if(key=="x"||key=="X")
-                        exit(0);
+//tam dung tro choi
+void pause_game(Race_Car &car)
+{
+    while(car.getStop())
+    {
+        clrscr();
+        cout<<"Nhan phim\"A\"de tiep tuc phim \"X\" de thoat\n\n Nhan Enter de xac nhan"<<endl;
+        string key;
+        fflush(stdin);
+        cin>>key;
+        if(key=="a"||key=="A")
+        {
+            //in ra thong tin hight neu co
+            if(car.getSovcquaMax()!=0)
+            {
+                gotoXY(47,11);
+                cout<<"SO VAT CAN QUA NHIEU NHAT:"<<car.getSovcquaMax();
             }
-
+            //Xac nhan viec thay doi gia tri de in ra man hinh
+            car.setAff( true,0);
+            car.setAff(true,1);
+            car.setAff(true,2);
+            //ket thuc viec tam dung chuong trinh
+            car.setStop(false);
+        }
+        else if(key=="x"||key=="X")
+        {
+            TextColor(10);
+            cout<<"\nGAME OVER!"<<endl;
+            exit(0);
+        }
+    }
 }
+
 
